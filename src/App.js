23ee9a9f45925cd2dcab000e46621232fcdb6019
AlanSoto31 <soto.alan@hotmail.com>
2021-07-21 16:15:39 -0500

    Display requested data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
           "id": 1,
           "name": "\"bulbasaur\"",
           "img_url": null,
-          "types": null,
+          "types": "[{\"slot\":1,\"type\":{\"name\":\"grass\",\"url\":\"https://pokeapi.co/api/v2/type/12/\"}},{\"slot\":2,\"type\":{\"name\":\"poison\",\"url\":\"https://pokeapi.co/api/v2/type/4/\"}}]",
           "weight": null,
           "abilities": "[{\"ability\":{\"name\":\"default\",\"url\":\"https://pokeapi.co/api/v2/ability/65/\"},\"is_hidden\":false,\"slot\":1},{\"ability\":{\"name\":\"default\",\"url\":\"https://pokeapi.co/api/v2/ability/34/\"},\"is_hidden\":true,\"slot\":3}]",
           "description": null,
@@ -19,7 +19,7 @@ const App = () => {
           "id": 2,
           "name": "\"ivysaur\"",
           "img_url": null,
-          "types": null,
+          "types": "[{\"slot\":1,\"type\":{\"name\":\"grass\",\"url\":\"https://pokeapi.co/api/v2/type/12/\"}},{\"slot\":2,\"type\":{\"name\":\"poison\",\"url\":\"https://pokeapi.co/api/v2/type/4/\"}}]",
           "weight": null,
           "abilities": "[{\"ability\":{\"name\":\"default\",\"url\":\"https://pokeapi.co/api/v2/ability/65/\"},\"is_hidden\":false,\"slot\":1},{\"ability\":{\"name\":\"default\",\"url\":\"https://pokeapi.co/api/v2/ability/34/\"},\"is_hidden\":true,\"slot\":3}]",
           "description": null,
@@ -40,12 +40,45 @@ const App = () => {
     fetchData();
   }, []);
 
-  pokemonsList.map( item => (
-    console.log(item.name)
+  const names = pokemonsList.map( item => (
+    <li key={item.name}>{item.name}</li>
   ))
 
+  const typesNameArr = pokemonsList.map( pokemon => {
+    const typesName = JSON.parse(pokemon.types).map( typesArrPerPokemon => typesArrPerPokemon.type.name)
+    return typesName
+  })
+
+  console.log(typesNameArr)
+
+  const types = typesNameArr.map( item => (
+    <li key={Math.random()}><span>{item[0]}</span><span> | </span><span>{item[1]}</span></li>
+  )) 
+
+  const abilitiesNameArr = pokemonsList.map( pokemon => {
+    const abilitiesName = JSON.parse(pokemon.abilities).map( abilitiesArrPerPokemon => abilitiesArrPerPokemon.ability.name)
+    return abilitiesName 
+  })
+
+  const abilities = abilitiesNameArr.map( item => (
+    <li key={Math.random()}><span>{item[0]}</span><span> | </span><span>{item[1]}</span></li>
+  )) 
+
   return (
-    <h1>Hello</h1>
+    <>
+      <ul>
+      <h1>Names</h1>
+         {names}
+      </ul>
+      <ul>
+      <h1>Abilities</h1>
+         {abilities}
+      </ul>
+      <ul>
+      <h1>Types</h1>
+         {types}
+      </ul>
+    </>
   );
 }
 
