f2b3f81c52162903300b580cfb48ad118489dcc5
AlanSoto31 <soto.alan@hotmail.com>
2021-07-22 10:22:12 -0500

    Display pokemon detailed info in detail component

--- src/Containers/PokemonDet.js
+++ src/Containers/PokemonDet.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const PokemonDet = () => {
   const { id } = useParams();
+  const pokemonsList = useSelector((state) => state.pokemons.list);
+
+  const pokemonDet = pokemonsList.find((pokemon) => pokemon.id === parseInt(id, 10));
+  // console.log(pokemonDet);
+
   return (
     <div>
-      {`This is the pokemon number ${id}`}
+      {`This is ${pokemonDet.name}`}
     </div>
   );
 };
