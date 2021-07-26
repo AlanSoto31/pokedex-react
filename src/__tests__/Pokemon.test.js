0285b59cb71f82ac83d9d7c93ea369db17b17fe5
AlanSoto31 <soto.alan@hotmail.com>
2021-07-26 08:34:56 -0500

    Add the first test to make sure testing environment works

---
+++ src/__tests__/Pokemon.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Pokemon from '../Components/Pokemon';
+
+describe('<Pokemon/>', () => {
+  let component;
+  const pokemon1 = {
+    img_url_1: '', name: 'charmander', description: '', weight: '', types: '[]', abilities: '[]', evolutions: '[]',
+};
+
+  beforeEach(() => {
+    component = render(
+      <BrowserRouter>
+        <Pokemon pokemon={pokemon1} />
+      </BrowserRouter>,
+    );
+  });
+
+  test('Renders content', () => {
+    component.getByText('charmander');
+  });
+});
